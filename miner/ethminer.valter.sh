aabaa2bdad30651786295474b9a94a47630d6f73
discoroveryx <discoroveryx@gmail.com>
2020-10-02 13:47:10 -0400

    autocommit_from_discoroveryx

--- miner/ethminer.valter.sh
+++ miner/ethminer.valter.sh
@@ -5,7 +5,7 @@
 
 # SSL connection: asia1 eu1 us1 us2
 /home/user/miner/ethminer/bin/ethminer \
--P stratum1+ssl://0xE8068577d46A1028f5f0D289f9df4fbcC7A06ff7.walter@asia1.ethermine.org:5555 \
+-P stratum1+ssl://0xE8068577d46A1028f5f0D289f9df4fbcC7A06ff7.valter@asia1.ethermine.org:5555 \
 --HWMON 1 \
 --tstart 65 --tstop 85 \
 --display-interval 2 \
