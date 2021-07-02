220170be5bd63474233d7337cb3fe47ca8a3090f
discoroveryx <discoroveryx@gmail.com>
2021-07-03 01:26:55 +0700

    autocommit_from_discoroveryx

--- chia/create_plot_2.sh
+++ chia/create_plot_2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tmux new-session -s create_plot_2 '/bin/bash chia plots create -k 32 -n 3 -t /home/chia/xch_hot -2 /home/chia/xch_hot -d /home/chia/xch_cold_1'
+tmux new-session -s create_plot_2 'cd /home/user/git/chia-blockchain && . ./activate && chia version'
