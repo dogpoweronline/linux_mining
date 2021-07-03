ff4937cb164159f1483088bc98d11932753cc424
discoroveryx <discoroveryx@gmail.com>
2021-07-03 16:56:47 +0700

    autocommit_from_discoroveryx

--- chia/farm_summary.sh
+++ chia/farm_summary.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-array=(seno spare chia flax chaingreen)
+array=(seno spare flax chaingreen chia)
 
 for item in ${array[*]}
 do
     printf "%s-----------------------\n" $item
     . /home/user/git/$item-blockchain/activate && $item farm summary
 done
+
+read -n 1 -s -r -p "Press any key to continue"
--- chia/temperature_nvme0n1.sh
+++ chia/temperature_nvme0n1.sh
@@ -3,3 +3,5 @@
 # apt install -t smartmontools
 
 smartctl -a /dev/nvme0n1 | grep Temperature
+
+read -n 1 -s -r -p "Press any key to continue"
